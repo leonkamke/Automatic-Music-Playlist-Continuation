d451830b7c665eb7b97fda785c143f26e84dfab9
Leon <64018777+LeonFuchs@users.noreply.github.com>
2022-05-13 21:54:39 +0200

    bug fixes

--- evaluation/eval.py
+++ evaluation/eval.py
@@ -34,7 +34,9 @@ def evaluate_model(model, word2vec_tracks, word2vec_artists, start_idx, end_idx,
         print("playlist " + str(i) + " of " + str(len(evaluation_dataset)) + " -----------------")
         # src (list of indices), trg (list of indices)
         src = src.to(device)
+        print("a")
         prediction = model.predict(src, len(trg))
+        print("b")
 
         # prediction is of shape len(trg)
         # first compute R-Precision and NDCG for tracks
