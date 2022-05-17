c1a5d989398328b31a65fe0b921b2b5b722d8a9d
Leon <l.kamke@web.de>
2022-05-17 19:09:00 +0200

    results will be written into a file

--- evaluation/eval.py
+++ evaluation/eval.py
@@ -31,7 +31,7 @@ def evaluate_model(model, word2vec_tracks, word2vec_artists, start_idx, end_idx,
     for i, (src, trg) in enumerate(evaluation_dataset):
         print("playlist " + str(i) + " of " + str(len(evaluation_dataset)) + " -----------------")
         # src (list of indices), trg (list of indices)
-        # src = src.to(device)
+        src = src.to(device)
         print("a")
         num_predictions = len(trg)
         # num_predictions = 500
