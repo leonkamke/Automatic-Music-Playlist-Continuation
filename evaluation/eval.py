c5eaa3a05dfe5c327f879920c3e5141711cdeae8
Leon <l.kamke@web.de>
2022-05-19 16:03:24 +0200

    small changes

--- evaluation/eval.py
+++ evaluation/eval.py
@@ -30,18 +30,16 @@ def evaluate_model(model, word2vec_tracks, word2vec_artists, start_idx, end_idx,
 
     for i, (src, trg) in enumerate(evaluation_dataset):
         print("playlist " + str(i) + " of " + str(len(evaluation_dataset)) + " -----------------")
+        print("length playlist: " + str(len(src)+len(trg)))
         # src (list of indices), trg (list of indices)
         src = src.to(device)
-        print("a")
         num_predictions = len(trg)
         # num_predictions = 500
         prediction = model.predict(src, num_predictions)
-        print("b")
 
         # prediction is of shape len(trg)
         # first compute R-Precision and NDCG for tracks
         r_precision_tracks = calc_r_precision(prediction, trg)
-        print("c")
         ndcg_tracks = calc_ndcg(prediction, trg)
         r_precision_tracks_sum += r_precision_tracks
         ndcg_tracks_sum += ndcg_tracks
