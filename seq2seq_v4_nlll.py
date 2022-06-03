cc0a018ce81823ca4cce9d7db79efcce82e93ef1
Leon <64018777+LeonFuchs@users.noreply.github.com>
2022-06-03 23:23:38 +0200

    small changes

--- seq2seq_v4_nlll.py
+++ seq2seq_v4_nlll.py
@@ -72,7 +72,7 @@ class Seq2Seq(nn.Module):
         # top_k.shape == (num_predictions)
         return top_k
 
-    def predict(self, input, num_predictions):
+    def predict_do_summed_rank(self, input, num_predictions):
         # input.shape == seq_len
         x, _ = self.forward(input)
         # x.shape == (seq_len, vocab_size)
@@ -82,7 +82,7 @@ class Seq2Seq(nn.Module):
         # top_k.shape == (num_predictions)
         return top_k
 
-    def predict_(self, input, num_predictions):
+    def predict(self, input, num_predictions):
         # input.shape == seq_len
         outputs = torch.zeros(num_predictions)
         outputs_vectors = torch.zeros(num_predictions, self.vocab_size)
