96682fd0363c7160e356f55ff11983d71aea8423
Leon <64018777+LeonFuchs@users.noreply.github.com>
2022-06-03 15:44:35 +0200

    small changes

--- evaluation/eval.py
+++ evaluation/eval.py
@@ -33,6 +33,7 @@ def evaluate_model(model, word2vec_tracks, word2vec_artists, start_idx, end_idx,
         print("length playlist: " + str(len(src)+len(trg)))
         # src (list of indices), trg (list of indices)
         src = src.to(device)
+        trg = trg.to(device)
         num_predictions = len(trg)
         num_predictions = 500
         prediction = model.predict(src, num_predictions)
