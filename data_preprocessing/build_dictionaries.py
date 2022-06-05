17a4c3709af756b8755cd5e496a559080fdfeecf
Leon <64018777+LeonFuchs@users.noreply.github.com>
2022-06-06 00:50:33 +0200

    Started implementing autoencoder.py

--- data_preprocessing/build_dictionaries.py
+++ data_preprocessing/build_dictionaries.py
@@ -20,5 +20,21 @@ def build_track_uri2id():
     print("finished")
 
 
+def build_id2track_uri():
+    print("build track_uri2id dict")
+    track2vec = gensim.models.Word2Vec.load('/netscratch/kamke/models/word2vec/1_mil_playlists/word2vec-song-vectors.model')
+    dict = {}
+    for i in range(len(track2vec.wv)):
+        print(str(i))
+        id = i
+        uri = track2vec.wv.index_to_key[id]
+        dict[id] = uri
+    with open(OUTPUT_PATH + 'id2track_uri.pkl', 'wb') as f:
+        pickle.dump(dict, f)
+    print("finished")
+
+
 if __name__ == "__main__":
-    build_track_uri2id()
+    with open('saved_dictionary.pkl', 'rb') as f:
+        loaded_dict = pickle.load(f)
+        print(loaded_dict[2262291])
