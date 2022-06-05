fab27f5f5ef9cf4ff5aa9248824558b14e6ae00d
Leon <64018777+LeonFuchs@users.noreply.github.com>
2022-06-06 01:18:22 +0200

    Started implementing autoencoder.py

--- data_preprocessing/build_dictionaries.py
+++ data_preprocessing/build_dictionaries.py
@@ -1,3 +1,4 @@
+import csv
 import pickle
 
 import gensim
@@ -34,7 +35,91 @@ def build_id2track_uri():
     print("finished")
 
 
+def build_track_id2reduced_track_id():
+    print("build track_id 2 reduced_track_id")
+    track2vec = gensim.models.Word2Vec.load('/netscratch/kamke/models/word2vec/1_mil_playlists_reduced/word2vec-song-vectors.model')
+    dict = {}
+    for i in range(len(track2vec.wv)):
+        print(str(i))
+        id = i
+        uri = track2vec.wv.index_to_key[id]
+        dict[id] = uri
+    with open(OUTPUT_PATH + 'trackid2reduced_trackid.pkl', 'wb') as f:
+        pickle.dump(dict, f)
+    print("finished")
+
+
+def build_artist_uri2id():
+    print("build artist_uri2id dict")
+    word2vec = gensim.models.Word2Vec.load('/netscratch/kamke/models/word2vec/1_mil_playlists_artists/word2vec-song-vectors.model')
+    dict = {}
+    for i in range(len(word2vec.wv)):
+        print(str(i))
+        id = i
+        uri = word2vec.wv.index_to_key[id]
+        dict[uri] = id
+    with open(OUTPUT_PATH + 'artist_uri2id.pkl', 'wb') as f:
+        pickle.dump(dict, f)
+    print("finished")
+
+
+def build_album_uri2id():
+    print("build artist_uri2id dict")
+    word2vec = gensim.models.Word2Vec.load('/netscratch/kamke/models/word2vec/1_mil_playlists_albums/word2vec-song-vectors.model')
+    dict = {}
+    for i in range(len(word2vec.wv)):
+        print(str(i))
+        id = i
+        uri = word2vec.wv.index_to_key[id]
+        dict[uri] = id
+    with open(OUTPUT_PATH + 'albums_uri2id.pkl', 'wb') as f:
+        pickle.dump(dict, f)
+    print("finished")
+
+
+def build_trackid2artistid():
+    print("build trackid2artistid")
+    track2vec = gensim.models.Word2Vec.load('/netscratch/kamke/models/word2vec/1_mil_playlists/word2vec-song-vectors.model')
+    artist2vec = gensim.models.Word2Vec.load('/netscratch/kamke/models/word2vec/1_mil_playlists_artists/word2vec-song-vectors.model')
+    with open('/ds/audio/MPD/spotify_million_playlist_dataset_csv/data/track_artist_dict_unique.csv', encoding='utf8') as read_obj:
+        csv_reader = csv.reader(read_obj)
+        # Iterate over each row in the csv file and create dictionary of track_uri -> artist_uri
+        track_artist_dict = {}
+        for index, row in enumerate(csv_reader):
+            if row[0] not in track_artist_dict:
+                track_id = track2vec.wv.get_index(row[0])
+                artist_id = artist2vec.wv.get_index(row[1])
+                track_artist_dict[track_id] = artist_id
+    with open(OUTPUT_PATH + 'trackid2artistid.pkl', 'wb') as f:
+        pickle.dump(dict, f)
+    print("finished")
+
+
+def build_trackid2albumid():
+    print("build trackid2artistid")
+    track2vec = gensim.models.Word2Vec.load('/netscratch/kamke/models/word2vec/1_mil_playlists/word2vec-song-vectors.model')
+    album2vec = gensim.models.Word2Vec.load('/netscratch/kamke/models/word2vec/1_mil_playlists_albums/word2vec-song-vectors.model')
+    with open('/ds/audio/MPD/spotify_million_playlist_dataset_csv/data/track_album_dict_unique.csv', encoding='utf8') as read_obj:
+        csv_reader = csv.reader(read_obj)
+        # Iterate over each row in the csv file and create dictionary of track_uri -> artist_uri
+        track_album_dict = {}
+        for index, row in enumerate(csv_reader):
+            if row[0] not in track_album_dict:
+                track_id = track2vec.wv.get_index(row[0])
+                artist_id = album2vec.wv.get_index(row[1])
+                track_album_dict[track_id] = artist_id
+    with open(OUTPUT_PATH + 'trackid2albumid.pkl', 'wb') as f:
+        pickle.dump(dict, f)
+    print("finished")
+
+
+#track_all_id2artist_id
+#track_all_id2album_id
 if __name__ == "__main__":
-    with open(OUTPUT_PATH + 'track_uri2id.pkl', 'rb') as f:
-        loaded_dict = pickle.load(f)
-        print(loaded_dict["spotify:track:6U7hJoo1kiTZI69cEF8uCD"])
+    build_track_uri2id()
+    build_id2track_uri()
+    build_track_id2reduced_track_id()
+    build_artist_uri2id()
+    build_album_uri2id()
+    build_trackid2artistid()
+    build_trackid2albumid()
