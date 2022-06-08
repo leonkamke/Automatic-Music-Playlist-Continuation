99027db17a586cf445c4e8289c1d3f87e66df685
Leon <l.kamke@web.de>
2022-06-08 20:14:20 +0200

    small changes

--- ensemble.py
+++ ensemble.py
@@ -5,7 +5,8 @@ from autoencoder import Autoencoder
 import track_embeddings
 from evaluation import eval
 from data_preprocessing import load_data as ld
-
+import torch.nn as nn
+from seq2seq_v4_nll_reduced import Seq2Seq
 import seq2seq_v3_track_album_artist
 
 
@@ -28,10 +29,12 @@ class Ensemble:
             prediction = model.predict(input, num_predictions)
             for i, track_id in enumerate(prediction):
                 track_id = int(track_id)
+                i = int(i)
                 rankings[track_id] += (num_predictions - i)
 
         _, top_k = torch.topk(rankings, dim=0, k=num_predictions)
-        rankings = torch.zeros(self.vocab_size, dtype=torch.float)
+
+        """rankings = torch.zeros(self.vocab_size, dtype=torch.float)
 
         for track_id in top_k:
             track_id = int(track_id)
@@ -39,7 +42,7 @@ class Ensemble:
             popularity = self.track2vec.wv.get_vecattr(track_uri, "count")
             rankings[track_id] = popularity
 
-        _, top_k = torch.topk(rankings, dim=0, k=num_predictions)
+        _, top_k = torch.topk(rankings, dim=0, k=num_predictions)"""
         return top_k
 
 
@@ -63,17 +66,17 @@ if __name__ == "__main__":
     trackId2reducedAlbumId = ld.get_trackid2reduced_albumid()
     print("loaded dictionaries from file")
 
-    print("create word2vec model for ensemble")
+    """print("create word2vec model for ensemble")
     model_word2vec = track_embeddings.Word2VecModel(word2vec_tracks)
     model_list.append(model_word2vec)
-    print("finished")
+    print("finished")"""
+
     # 15, 12
     print("create autoencoder for ensemble")
     NUM_TRACKS = len(reducedTrackUri2reducedId)
     NUM_ARTISTS = len(reducedArtistUri2reducedId)
     NUM_ALBUMS = len(reducedAlbumUri2reducedId)
     HID_DIM = 256
-
     save_file_name = "/autoencoder.pth"
     # (self, hid_dim, num_tracks, num_artists, num_albums, trackId2reducedTrackId, trackId2reducedArtistId,
     #                  reducedTrackId2trackId)
@@ -81,7 +84,18 @@ if __name__ == "__main__":
                               trackId2reducedArtistId, trackId2reducedAlbumId, reduced_trackId2trackId)
     autoencoder.load_state_dict(torch.load(la.output_path_model() + la.get_folder_name() + save_file_name))
     model_list.append(autoencoder)
-    print("created model")
+    print("finished")
+
+    print("create seq2seq model for ensemble")
+    weights_path = la.path_embedded_weights()
+    seq2seq_path = la.output_path_model() + "tracks2rec/seq2seq_v4_reduced_nll.pth"
+    weights = torch.load(weights_path, map_location=device)
+    # weights.shape == (2262292, 300)
+    # pre_trained embedding reduces the number of trainable parameters from 34 mill to 17 mill
+    embedding_pre_trained = nn.Embedding.from_pretrained(weights)
+    model = Seq2Seq(reduced_trackId2trackId, NUM_TRACKS, embedding_pre_trained, 256, 1)
+    model.load_state_dict(torch.load(seq2seq_path))
+    model_list.append(model)
     print("finished")
 
     print("model_list.len = ", len(model_list))
