06f971396a054adfa8352a934a5231b983c463ad
Leon <l.kamke@web.de>
2022-06-08 10:47:06 +0200

    small changes

--- data_preprocessing/build_character_vocab.py
+++ data_preprocessing/build_character_vocab.py
@@ -4,8 +4,17 @@ we deal with 41 characters including 26 alphabets (a-z), 10 numbers
 (0-9), and 5 special characters (/<>+-)
 """
 import csv
+import pickle
+
+import numpy as np
 
 PATH_TRACK_SEQUENCES = '/ds/audio/MPD/spotify_million_playlist_dataset_csv/data/track_sequences.csv'
+OUTPUT_PATH = "/netscratch/kamke/dictionaries/"
+
+VOCABULARY = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n',     # 26 alphabets
+              'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+              '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',                         # 10 numbers
+              '+', '-', '/', '<', '>', '?', '!']                                        # 7 special characters
 
 
 def write_all_titles_in_csv():
@@ -23,5 +32,26 @@ def write_all_titles_in_csv():
     print("read ", counter, " titles")
 
 
+def convert_title(title_str):
+    output = title_str.lower()
+    title_str = title_str.lower()
+    for char in title_str:
+        if char not in VOCABULARY:
+            output = output.replace(char, "")
+    return output
+
+
+def build_one_hot_encoded_dict():
+    vocab_size = len(VOCABULARY)
+    char_to_vec = {}
+    for i in range(vocab_size):
+        char = VOCABULARY[i]
+        vector = np.zeros((vocab_size,), dtype=int)
+        vector[i] = 1
+        char_to_vec[char] = vector
+    with open(OUTPUT_PATH + 'char_dict.pkl', 'wb') as f:
+        pickle.dump(char_to_vec, f)
+
+
 if __name__ == "__main__":
-    write_all_titles_in_csv()
+    build_one_hot_encoded_dict()
--- title2rec.py
+++ title2rec.py
@@ -1,24 +1,9 @@
-import gensim
-import spacy
-import en_core_web_trf
+import torch
+import torch.nn as nn
+import numpy
+from data_preprocessing import build_character_vocab as cv
+
+
 
 
 if __name__ == "__main__":
-    """print("load FastText model")
-    fasttext_model = gensim.models.fasttext.load_facebook_vectors("models/fasttext/cc.en.300.bin")
-    print("loaded model")
-    word1 = "RAVE"
-    word2 = "party playlist"
-    word3 = "New School"
-    print(fasttext_model.similarity("party", "party playlist"))
-    print(fasttext_model.similar_by_vector(word1))
-    print(fasttext_model.similar_by_vector(word2))
-    print(fasttext_model.similar_by_vector(word3))
-    """
-    spacy.prefer_gpu()
-    nlp = en_core_web_trf.load()
-    doc1 = nlp("I like salty fries and hamburgers.")
-    doc2 = nlp("Fast food tastes very good.")
-
-    # Similarity of two documents
-    print(doc1, "<->", doc2, doc1.similarity(doc2))
