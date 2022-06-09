afd6dc4e25deb499992891fc2c59f2933ec865e7
Leon <l.kamke@web.de>
2022-06-10 00:01:11 +0200

    small changes

--- ensemble.py
+++ ensemble.py
@@ -118,10 +118,70 @@ class EnsembleRecall:
         self.vocab_size = 2262292
         self.num_tracks = num_tracks
 
+        self.track2vec = track2vec
+        # number of unique tracks in the MPD dataset = 2262292
+        self.vocab_size = 2262292
+
+        # -----------------------------------------------------------------------------------------------
+        model_list = []
+
+        device = torch.device("cpu")
+
+        """print("load word2vec models")
+        word2vec_tracks = gensim.models.Word2Vec.load(la.path_track_to_vec_model())
+        print("finished")"""
+
+        print("load dictionaries from file")
+        reducedTrackUri2reducedId = ld.get_reducedTrackUri2reducedTrackID()
+        reducedArtistUri2reducedId = ld.get_reducedArtistUri2reducedArtistID()
+        reducedAlbumUri2reducedId = ld.get_reducedAlbumUri2reducedAlbumID()
+        reduced_trackId2trackId = ld.get_reduced_trackid2trackid()
+        trackId2reducedTrackId = ld.get_trackid2reduced_trackid()
+        trackId2reducedArtistId = ld.get_trackid2reduced_artistid()
+        trackId2reducedAlbumId = ld.get_trackid2reduced_albumid()
+        print("loaded dictionaries from file")
+
+        """print("create word2vec model for ensemble")
+        model_word2vec = track_embeddings.Word2VecModel(word2vec_tracks)
+        model_list.append(model_word2vec)
+        print("finished")"""
+
+        # 15, 12
+        print("create autoencoder for ensemble")
+        NUM_TRACKS = len(reducedTrackUri2reducedId)
+        NUM_ARTISTS = len(reducedArtistUri2reducedId)
+        NUM_ALBUMS = len(reducedAlbumUri2reducedId)
+        HID_DIM = 256
+        save_file_name = "/autoencoder.pth"
+        # (self, hid_dim, num_tracks, num_artists, num_albums, trackId2reducedTrackId, trackId2reducedArtistId,
+        #                  reducedTrackId2trackId)
+        autoencoder = Autoencoder(HID_DIM, NUM_TRACKS, NUM_ARTISTS, NUM_ALBUMS, trackId2reducedTrackId,
+                                  trackId2reducedArtistId, trackId2reducedAlbumId, reduced_trackId2trackId)
+        autoencoder.load_state_dict(torch.load(la.output_path_model() + "/autoencoder_1" + save_file_name))
+        autoencoder.to(device)
+        autoencoder.eval()
+        model_list.append(autoencoder)
+        print("finished")
+
+        print("create seq2seq model for ensemble")
+        weights_path = la.path_embedded_weights()
+        seq2seq_path = la.output_path_model() + "/tracks2rec/seq2seq_v4_reduced_nll.pth"
+        weights = torch.load(weights_path, map_location=device)
+        # weights.shape == (2262292, 300)
+        # pre_trained embedding reduces the number of trainable parameters from 34 mill to 17 mill
+        embedding_pre_trained = nn.Embedding.from_pretrained(weights)
+        seq2seq = Seq2Seq(reduced_trackId2trackId, NUM_TRACKS, embedding_pre_trained, 256, 1)
+        seq2seq.load_state_dict(torch.load(seq2seq_path))
+        seq2seq.to(device)
+        seq2seq.eval()
+        print("finished")
+
+        model_list.append(seq2seq)
+        self.model_list = model_list
+
     def predict(self, input, num_predictions):
-        pred_autoencoder = self.autoencoder.predict(input, 5000)
+        pred_autoencoder = self.autoencoder.predict(input, 1000)
         # pred_autoencoder = sequence of track id's
-        # red_pred_auto = sequence of reduced track id's
         pred_seq2seq, _ = self.seq2seq.forward(input)
         # pred_seq2seq.shape = (seq_len, num_tracks)
         pred_seq2seq = pred_seq2seq[-1]
@@ -134,7 +194,7 @@ class EnsembleRecall:
 
         _, top_k = torch.topk(rankings, dim=0, k=num_predictions, largest=False)
 
-        rankings = torch.zeros(self.vocab_size, dtype=torch.float)
+        """rankings = torch.zeros(self.vocab_size, dtype=torch.float)
 
         # sort corresponding to popularity
         for track_id in top_k:
@@ -143,14 +203,14 @@ class EnsembleRecall:
             popularity = self.track2vec.wv.get_vecattr(track_uri, "count")
             rankings[track_id] = popularity
 
-        _, top_k = torch.topk(rankings, dim=0, k=num_predictions)
+        _, top_k = torch.topk(rankings, dim=0, k=num_predictions)"""
 
         output = []
         for trackId in top_k:
             output.append(int(trackId))
         # output has to be a list of track_id's
         # outputs.shape == (num_predictions)"""
-        return self.seq2seq.predict(input, num_predictions)
+        return output
 
 
 if __name__ == "__main__":
