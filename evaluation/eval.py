7a57999fcf09b0ce2e7dba7d086fb9853ce4be60
Leon <64018777+LeonFuchs@users.noreply.github.com>
2022-06-10 11:00:14 +0200

    Started implementing autoencoder.py

--- evaluation/eval.py
+++ evaluation/eval.py
@@ -274,7 +274,8 @@ def playlist_extender_clicks(predictions, targets, max_n_predictions=500):
     # Assumes predictions are sorted by relevance
     # First, cap the number of predictions
     predictions = predictions[:max_n_predictions]
-
+    print(set(predictions))
+    print(set(targets))
     # Calculate metric
     i = set(predictions).intersection(set(targets))
     for index, t in enumerate(predictions):
