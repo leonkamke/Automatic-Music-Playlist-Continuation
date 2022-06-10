7e511bdd8101cb87aed2a257503012e50b730766
Leon <l.kamke@web.de>
2022-06-10 15:43:48 +0200

    small changes

--- playlist_continuation/ensemble/ensemble.py
+++ playlist_continuation/ensemble/ensemble.py
@@ -29,12 +29,12 @@ class Ensemble:
         trackId2reducedArtistId = ld.get_trackid2reduced_artistid()
         trackId2reducedAlbumId = ld.get_trackid2reduced_albumid()
         print("loaded dictionaries from file")
-
-        # 15, 12
-        print("create autoencoder for ensemble")
         NUM_TRACKS = len(reducedTrackUri2reducedId)
         NUM_ARTISTS = len(reducedArtistUri2reducedId)
         NUM_ALBUMS = len(reducedAlbumUri2reducedId)
+
+        # 15, 12
+        """print("create autoencoder for ensemble")
         HID_DIM = 256
         save_file_name = "/autoencoder.pth"
         # (self, hid_dim, num_tracks, num_artists, num_albums, trackId2reducedTrackId, trackId2reducedArtistId,
@@ -45,7 +45,7 @@ class Ensemble:
         autoencoder.to(device)
         autoencoder.eval()
         model_list.append(autoencoder)
-        print("finished")
+        print("finished")"""
 
         print("create seq2seq model for ensemble")
         weights_path = la.path_embedded_weights()
@@ -215,68 +215,9 @@ class EnsembleRecall:
 
 if __name__ == "__main__":
     device = torch.device("cpu")
-
     print("load word2vec models")
     word2vec_tracks = gensim.models.Word2Vec.load(la.path_track_to_vec_model())
     print("finished")
-
-    """
-    print("load dictionaries from file")
-    reducedTrackUri2reducedId = ld.get_reducedTrackUri2reducedTrackID()
-    reducedArtistUri2reducedId = ld.get_reducedArtistUri2reducedArtistID()
-    reducedAlbumUri2reducedId = ld.get_reducedAlbumUri2reducedAlbumID()
-    reduced_trackId2trackId = ld.get_reduced_trackid2trackid()
-    trackId2reducedTrackId = ld.get_trackid2reduced_trackid()
-    trackId2reducedArtistId = ld.get_trackid2reduced_artistid()
-    trackId2reducedAlbumId = ld.get_trackid2reduced_albumid()
-    print("loaded dictionaries from file")
-
-    print("create word2vec model for ensemble")
-    model_word2vec = track_embeddings.Word2VecModel(word2vec_tracks)
-    model_list.append(model_word2vec)
-    print("finished")
-
-    # 15, 12
-    print("create autoencoder for ensemble")
-    NUM_TRACKS = len(reducedTrackUri2reducedId)
-    NUM_ARTISTS = len(reducedArtistUri2reducedId)
-    NUM_ALBUMS = len(reducedAlbumUri2reducedId)
-    HID_DIM = 256
-    save_file_name = "/autoencoder.pth"
-    # (self, hid_dim, num_tracks, num_artists, num_albums, trackId2reducedTrackId, trackId2reducedArtistId,
-    #                  reducedTrackId2trackId)
-    autoencoder = Autoencoder(HID_DIM, NUM_TRACKS, NUM_ARTISTS, NUM_ALBUMS, trackId2reducedTrackId,
-                              trackId2reducedArtistId, trackId2reducedAlbumId, reduced_trackId2trackId)
-    autoencoder.load_state_dict(torch.load(la.output_path_model() + "/autoencoder_1" + save_file_name))
-    autoencoder.eval()
-    model_list.append(autoencoder)
-    print("finished")
-
-    print("create seq2seq model for ensemble")
-    weights_path = la.path_embedded_weights()
-    seq2seq_path = la.output_path_model() + "/tracks2rec/seq2seq_v4_reduced_nll.pth"
-    weights = torch.load(weights_path, map_location=device)
-    # weights.shape == (2262292, 300)
-    # pre_trained embedding reduces the number of trainable parameters from 34 mill to 17 mill
-    embedding_pre_trained = nn.Embedding.from_pretrained(weights)
-    seq2seq = Seq2Seq(reduced_trackId2trackId, NUM_TRACKS, embedding_pre_trained, 256, 1)
-    seq2seq.load_state_dict(torch.load(seq2seq_path))
-    seq2seq.eval()
-    model_list.append(seq2seq)
-    print("finished")
-
-    print("create title2rec model for ensemble")
-    NUM_TRACKS = len(reducedTrackUri2reducedId)
-    NUM_CHARS = 43
-    HID_DIM = 256
-    N_LAYERS = 1
-    title2rec = Title2Rec(NUM_CHARS, NUM_TRACKS, HID_DIM, N_LAYERS, reduced_trackId2trackId)
-    title2rec.eval()
-    # model_list.append(title2rec)
-    print("finished")
-
-    print("model_list.len = ", len(model_list))
-    """
     # create ensemble model
     # ensemble_model = EnsembleRecall(word2vec_tracks)
     ensemble_model = Ensemble(word2vec_tracks)
