7470f26664f3ed803ad1eb66b937c3601d68c670
Leon <64018777+LeonFuchs@users.noreply.github.com>
2022-06-23 19:38:14 +0200

    Started implementing autoencoder.py

--- playlist_continuation/ensemble/ensemble.py
+++ playlist_continuation/ensemble/ensemble.py
@@ -61,7 +61,7 @@ class Ensemble:
         # model_list.append(seq2seq)
         print("finished")"""
 
-        """print("create seq2seq_5 model for ensemble")
+        print("create seq2seq_5 model for ensemble")
         weights_path = la.path_embedded_weights()
         seq2seq_path = la.output_path_model() + "/tracks2rec_5/seq2seq_v4_reduced_nll.pth"
         weights = torch.load(weights_path, map_location=device)
@@ -73,7 +73,7 @@ class Ensemble:
         seq2seq_5.to(device)
         seq2seq_5.eval()
         # model_list.append(seq2seq_5)
-        print("finished")"""
+        print("finished")
 
         """print("create seq2seq_2 model for ensemble")
         weights_path = la.path_embedded_weights_tracks()
