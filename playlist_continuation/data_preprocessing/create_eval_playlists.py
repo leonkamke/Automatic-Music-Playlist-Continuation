5cdc0691e48154ff86a5cd488fc782bb7349395d
Leon <l.kamke@web.de>
2022-06-23 20:30:04 +0200

    small changes

--- playlist_continuation/data_preprocessing/create_eval_playlists.py
+++ playlist_continuation/data_preprocessing/create_eval_playlists.py
@@ -4,23 +4,41 @@ from playlist_continuation.config import load_attributes as la
 
 # training playlists:    0 - 980000
 # evaluation playlists:  980001 - 999999
+# --> evaluation_set contains of 20000 playlists
 start_idx = 980001
 end_idx = 999999
+# here are 13934 playlists, with length >= 30 tracks
 
 def create_dataset():
     with open(la.path_track_sequences_path(), encoding='utf8') as read_obj:
         csv_reader = csv.reader(read_obj)
         # Iterate over each row in the csv file and create lists of track uri's
 
-        c_title_only = 0
+        """c_title_only = 0
         c_first_5 = 0
         c_first_10 = 0
         c_first_25 = 0
-        c_first_100 = 0
+        c_first_100 = 0"""
+        num_150_ = 0
+        num_100_150 = 0
+        num_60_100 = 0
+        num_30_60 = 0
+
+
+
         num_playlists_more_30 = 0
         for index, row in enumerate(csv_reader):
-            if start_idx <= index < end_idx and len(row) >= 32:
+            if start_idx <= index <= end_idx and len(row) >= 32:
                 num_playlists_more_30 += 1
+                len_playlist = len(row) - 2
+                if len_playlist >= 150:
+                    num_150_ += 1
+                elif len_playlist >= 100:
+                    num_100_150 += 1
+                elif len_playlist >= 60:
+                    num_60_100 += 1
+                elif len_playlist >= 30:
+                    num_30_60 += 1
                 """is_odd = len(row) % 2 == 1
                 i = int(len(row) / 2 + 1)
                 pids.append(row[0])
@@ -33,7 +51,11 @@ def create_dataset():
                 titles.append(row[1])"""
             if index > end_idx:
                 break
-        print(num_playlists_more_30)
+        print("num playlists ", num_playlists_more_30)
+        print("num_150_ ", num_150_)
+        print("num_100_150 ", num_100_150)
+        print("num_60_100 ", num_60_100)
+        print("num_30_60 ", num_30_60)
 
 
 if __name__ == "__main__":
