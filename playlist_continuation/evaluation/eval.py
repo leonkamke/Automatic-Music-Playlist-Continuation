a7cf429210494e6ffb6646a99d17825dd2f95711
Leon <l.kamke@web.de>
2022-06-25 13:00:08 +0200

    small changes

--- playlist_continuation/evaluation/eval.py
+++ playlist_continuation/evaluation/eval.py
@@ -37,11 +37,11 @@ def spotify_evaluation(model, trackId2artistId, trackUri2trackId, artistUri2arti
     result_str = ""
 
     # title only
-    """r_precision, ndcg, clicks = eval_title_only(model, evaluation_dataset, device, trackId2artistId)
+    r_precision, ndcg, clicks = eval_title_only(model, evaluation_dataset, device, trackId2artistId)
     result_str += "Title only ---------------------------\n"
     result_str += "---> R-Precision            : " + str(r_precision) + "\n"
     result_str += "---> NDCG                   : " + str(ndcg) + "\n"
-    result_str += "---> Clicks                 : " + str(clicks) + "\n"""
+    result_str += "---> Clicks                 : " + str(clicks) + "\n
     r_precision_mean += 0
     ndcg_mean += 0
     clicks_mean += 51.0
