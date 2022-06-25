65daae89df3caaee856d40341f6aed5efcd2d520
Leon <l.kamke@web.de>
2022-06-25 16:46:14 +0200

    small changes

--- playlist_continuation/ensemble/ensemble.py
+++ playlist_continuation/ensemble/ensemble.py
@@ -59,7 +59,7 @@ class Ensemble:
         model_list.append(autoencoder)
         print("finished")
 
-        """print("create autoencoder_2 for ensemble")
+        print("create autoencoder_2 for ensemble")
         HID_DIM = 256
         save_file_name = "/autoencoder.pth"
         # (self, hid_dim, num_tracks, num_artists, num_albums, trackId2reducedTrackId, trackId2reducedArtistId,
@@ -72,6 +72,7 @@ class Ensemble:
         model_list.append(autoencoder)
         print("finished")
 
+        """
         print("create seq2seq model for ensemble")
         weights_path = la.path_embedded_weights()
         seq2seq_path = la.output_path_model() + "/tracks2rec/seq2seq_v4_reduced_nll.pth"
