b46c81e5e84423e117ce5a2511a46e566c7e9438
Leon <l.kamke@web.de>
2022-06-25 15:01:33 +0200

    small changes

--- playlist_continuation/ensemble/ensemble.py
+++ playlist_continuation/ensemble/ensemble.py
@@ -46,7 +46,7 @@ class Ensemble:
         self.title2rec = model
         print("finished")
 
-        print("create autoencoder_1 for ensemble")
+        """print("create autoencoder_1 for ensemble")
         HID_DIM = 256
         save_file_name = "/autoencoder.pth"
         # (self, hid_dim, num_tracks, num_artists, num_albums, trackId2reducedTrackId, trackId2reducedArtistId,
@@ -70,9 +70,9 @@ class Ensemble:
         autoencoder.to(device)
         autoencoder.eval()
         model_list.append(autoencoder)
-        print("finished")
+        print("finished")"""
 
-        """print("create seq2seq model for ensemble")
+        print("create seq2seq model for ensemble")
         weights_path = la.path_embedded_weights()
         seq2seq_path = la.output_path_model() + "/tracks2rec/seq2seq_v4_reduced_nll.pth"
         weights = torch.load(weights_path, map_location=device)
@@ -86,7 +86,7 @@ class Ensemble:
         model_list.append(seq2seq)
         print("finished")
 
-        print("create seq2seq_5 model for ensemble")
+        """print("create seq2seq_5 model for ensemble")
         weights_path = la.path_embedded_weights()
         seq2seq_path = la.output_path_model() + "/tracks2rec_5/seq2seq_v4_reduced_nll.pth"
         weights = torch.load(weights_path, map_location=device)
